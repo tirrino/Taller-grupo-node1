6bbf9503c3354e2d12aef76293701401134536f6
tirrino <tirrino2000@gmail.com>
2025-11-26 10:32:34 -0500

    feat: protect tasks routes with jwt middleware

--- src/routes/task.js
+++ src/routes/task.js
@@ -15,3 +15,11 @@ router.put('/:id', updateTask);
 router.delete('/:id', deleteTask);
 
 export default router;
+const auth = require('../middlewares/authMiddleware');
+
+router.get('/', auth, controller.listTasks);
+router.post('/', auth, controller.createTask);
+router.put('/:id', auth, controller.updateTask);
+router.delete('/:id', auth, controller.deleteTask);
+const tasks = await prisma.task.findMany({ where: { userId: req.user.id } });
+const task = await prisma.task.create({ data: { title, description, userId: req.user.id } });
